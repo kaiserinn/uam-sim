bf8b4238b2f3d9104c2843acbe7c7036db6403d5
kaiserinn <azharizqullah90@gmail.com>
2024-05-15 01:28:53 +0800

    feat: progress

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // contants
 let CANVAS_HEIGHT = window.innerHeight;
 let CANVAS_WIDTH = window.innerWidth;
-const GRAVITY = 1;
-const FRICTION = 0.005;
-const ELASTICITY = 0.9;
+const GRAVITY = 0.1;
+const FRICTION = -0.005;
+const RESTITUTION = 0.8;
 const colors = [
     "#dc8a78",
     "#dd7878",
@@ -27,6 +27,8 @@ let oldTimestamp = 0;
 let isPaused = true;
 let showHeading = false;
 let collisionEnabled = true;
+let selectedCircle = null;
+let inputIsFocused = false;
 
 /** @type HTMLCanvasElement */
 const canvas = document.getElementById("canvas");
@@ -45,51 +47,168 @@ window.addEventListener("resize", () => {
 const ctx = canvas.getContext("2d");
 
 class Circle {
-  constructor(x, y, r, strokeColor) {
+  constructor(x, y, r, color) {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.strokeColor = strokeColor || "black";
-    this.dx = 5;
-    this.dy = 1;
+    this.color = color || "black";
+    this.vx = 1;
+    this.vy = 0.1;
+    this.angle = 0;
     this.isHeld = false;
+    this.pos = {x: this.x, y: this.y};
+    this.lines = [
+      new Line({x: this.x, y: this.y}, {x: this.x, y: this.y - this.r}),
+      new Line({x: this.x, y: this.y}, {x: this.x, y: this.y + this.r}),
+      new Line({x: this.x, y: this.y}, {x: this.x - this.r, y: this.y}),
+      new Line({x: this.x, y: this.y}, {x: this.x + this.r, y: this.y})
+    ];
   }
 
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-    ctx.strokeStyle = "black"
+    ctx.strokeStyle = this.color;
     ctx.stroke();
-    ctx.fillStyle = this.strokeColor;
-    ctx.fill();
+    ctx.closePath();
+
+    this.lines.forEach(line => {
+      line.draw();
+    })
 
     if (showHeading) {
+      if (this.vx <= 0.1 && this.vy <= 0.1) return;
+      let vector = {x: this.x+this.vx - this.x, y: this.y+this.vy - this.y};
+      let distance = Math.sqrt(vector.x**2 + vector.y**2);
+      let normalizedVector = {x: vector.x / distance, y: vector.y / distance};
+      normalizedVector = {x: normalizedVector.x * 100, y: normalizedVector.y * 100};
+      vector = {x: normalizedVector.x + this.x, y: normalizedVector.y + this.y};
+
       ctx.beginPath();
       ctx.moveTo(this.x, this.y);
-      ctx.lineTo(this.x + this.dx * 10, this.y + this.dy * 10);
+      ctx.lineTo(vector.x, vector.y);
       ctx.stroke();
+      ctx.closePath();
     }
   }
 
   update() {
-    if (isPaused) {
-      return;
+    if (isPaused) return;
+
+    this.vx += this.vx * FRICTION;
+    this.x += this.vx;
+
+    this.vy += GRAVITY;
+    this.y += this.vy;
+
+    this.lineUpdate();
+
+    this.angle += (this.vx / this.r);
+    this.lines.forEach(line => {
+      line.update(this.angle);
+    })
+  }
+
+  lineUpdate() {
+    this.lines.forEach(line => {
+      line.vec1 = {x: this.x, y: this.y};  
+    })
+    this.lines[0].vec2 = {x: this.x, y: this.y - this.r};
+    this.lines[1].vec2 = {x: this.x, y: this.y + this.r};
+    this.lines[2].vec2 = {x: this.x - this.r, y: this.y};
+    this.lines[3].vec2 = {x: this.x + this.r, y: this.y};
+  }
+
+  collideWithWall() {
+    if (this.x - this.r < 0) {
+      this.x = this.r;
+      this.vx = -this.vx;
+      this.lineUpdate();
     }
+    if (this.x + this.r > CANVAS_WIDTH) {
+      this.x = CANVAS_WIDTH - this.r;
+      this.vx = -this.vx;
+      this.lineUpdate();
+    }
+    if (this.y - this.r < 0) {
+      this.y = this.r;
+      this.vy = -this.vy * RESTITUTION;
+    }
+    if (this.y + this.r > CANVAS_HEIGHT) {
+      this.y = CANVAS_HEIGHT - this.r;
+      this.vy = -this.vy * RESTITUTION;
+    }
+  }
 
-    this.dx -= this.dx * FRICTION;
-    this.x += this.dx;
+  inCircle(mousePos) {
+    return (this.x - mousePos.x)**2 + (this.y - mousePos.y)**2 <= this.r*this.r;
+  }
 
-    this.dy += GRAVITY;
-    this.y += this.dy;
+  collideBetweenCircle(other) {
+    const distBetween = (this.x - other.x)**2 + (this.y - other.y)**2;
+    return distBetween <= (this.r + other.r)*(this.r + other.r);
+  }
+
+  collideWithOtherBall(other) {
+    let vCollision = {x: other.x - this.x, y: other.y - this.y};
+    let distance = Math.sqrt((other.x-this.x)**2 + (other.y-this.y)**2);
+    let normalizedVCollision = {x: vCollision.x / distance, y: vCollision.y / distance};
+
+    let sumOfRadius = this.r + other.r;
+    let newVCollision = {x: normalizedVCollision.x * sumOfRadius, y: normalizedVCollision.y * sumOfRadius};
+    other.x = newVCollision.x + this.x;
+    other.y = newVCollision.y + this.y;
+
+    other.lineUpdate();
+
+    let vRelativeVelocity = {x: this.vx - other.vx, y: this.vy - other.vy};
+    let speed = vRelativeVelocity.x * normalizedVCollision.x + vRelativeVelocity.y * normalizedVCollision.y;
+    speed *= RESTITUTION;
+
+    if(speed < 0) return;
+
+    let impulse = 2 * speed / (sumOfRadius);
+
+    this.vx -= (impulse * other.r * normalizedVCollision.x);
+    this.vy -= (impulse * other.r * normalizedVCollision.y);
+    other.vx += (impulse * this.r * normalizedVCollision.x);
+    other.vy += (impulse * this.r * normalizedVCollision.y);
   }
 }
 
-const circleArr = [new Circle(100, 100, 50)];
+class Line {
+  constructor(vec1, vec2) {
+    this.vec1 = vec1;
+    this.vec2 = vec2;
+  }
 
-const pauseButtonEl = document.getElementById("pause_button");
-pauseButtonEl.addEventListener("click", () => {
-  isPaused = !isPaused;
-})
+  draw() {
+    ctx.beginPath();
+    ctx.moveTo(this.vec1.x, this.vec1.y);
+    ctx.lineTo(this.vec2.x, this.vec2.y);
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  update(w) {
+    this.vec2 = this.rotate(w);
+  }
+
+  rotate(w) {
+    let rotationVector = {x: this.vec1.x - this.vec2.x, y: this.vec1.y - this.vec2.y};
+    // let nextVec2X = rotationVector.x * Math.cos(w*Math.PI/180) - rotationVector.y * Math.sin(w*Math.PI/180);
+    // let nextVec2Y = rotationVector.x * Math.sin(w*Math.PI/180) + rotationVector.y * Math.cos(w*Math.PI/180);
+    let nextVec2X = rotationVector.x * Math.cos(w) - rotationVector.y * Math.sin(w);
+    let nextVec2Y = rotationVector.x * Math.sin(w) + rotationVector.y * Math.cos(w);
+    return {x: this.vec1.x - nextVec2X, y: this.vec1.y - nextVec2Y};
+  }
+}
+
+const circles = [new Circle(100, 100, 50)];
+
+const playButtonEl = document.getElementById("play-button");
+const pauseButtonEl = document.getElementById("pause-button");
+const pauseCheckboxEl = document.getElementById("play-pause");
 
 function getRandomIntInclusive(min, max) {
   const minCeiled = Math.ceil(min);
@@ -97,34 +216,33 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
-const addButtonEl = document.getElementById("add_button");
+const addButtonEl = document.getElementById("add-button");
 addButtonEl.addEventListener("click", () => {
+  if (circles.length > 30) return;
   const randomXPos = getRandomIntInclusive(50, CANVAS_WIDTH-50);
   const randomColor = colors[getRandomIntInclusive(0, colors.length-1)];
-  const randomRad = getRandomIntInclusive(30, 50);
-  circleArr.push(new Circle(randomXPos, 100, randomRad, randomColor));
+  circles.push(new Circle(randomXPos, 100, 50, randomColor));
 })
 
-const deleteButtonEl = document.getElementById("delete_button");
+const deleteButtonEl = document.getElementById("delete-button");
 deleteButtonEl.addEventListener("click", () => {
-  if (circleArr.length > 1) {
-    circleArr.shift();
+  if (selectedCircle && circles.length > 1) {
+    circles.splice(selectedCircle, 1);
+    selectedCircle = null;
+    return;
+  }
+  if (circles.length > 1) {
+    circles.shift();
   }
 })
 
-const optionsContainerEl = document.getElementById("options_container");
+const optionsContainerEl = document.getElementById("options-container");
 optionsContainerEl.style.display = "none";
 
-const showHeadingOptionEl = document.getElementById("show_headings");
-showHeadingOptionEl.addEventListener("change", () => {
-  showHeading = !showHeading;
-})
-const enableCollisionOptionEl = document.getElementById("collision_detection");
-enableCollisionOptionEl.addEventListener("change", () => {
-  collisionEnabled = !collisionEnabled;
-})
+const showHeadingOptionEl = document.getElementById("show-headings");
+const enableCollisionOptionEl = document.getElementById("collision-detection");
 
-const menuButtonEl = document.getElementById("menu_button");
+const menuButtonEl = document.getElementById("menu-button");
 menuButtonEl.addEventListener("click", () => {
   if (optionsContainerEl.style.display === "none") {
     optionsContainerEl.style.display = "flex";
@@ -133,125 +251,117 @@ menuButtonEl.addEventListener("click", () => {
   }
 })
 
-function collideWithWall(circle) {
-  if (circle.x - circle.r < 0) {
-    circle.x = circle.r;
-    circle.dx = -circle.dx;
-  }
-  if (circle.x + circle.r > CANVAS_WIDTH) {
-    circle.x = CANVAS_WIDTH - circle.r;
-    circle.dx = -circle.dx;
-  }
-  if (circle.y - circle.r < 0) {
-    circle.y = circle.r;
-    circle.dy = -circle.dy * ELASTICITY;
-  }
-  if (circle.y + circle.r > CANVAS_HEIGHT) {
-    circle.y = CANVAS_HEIGHT - circle.r;
-    circle.dy = -circle.dy * ELASTICITY;
-  }
-}
+const vxInputEl = document.getElementById("vx-input");
+vxInputEl.addEventListener("change", () => {
+  circles[selectedCircle].vx = Number(vxInputEl.value);
+})
+const vyInputEl = document.getElementById("vy-input");
+vyInputEl.addEventListener("change", () => {
+  circles[selectedCircle].vy = Number(vyInputEl.value);
+})
 
-function ballHitBall(ball1, ball2) {
-  let collision = false;
-  let dx = ball1.x - ball2.x;
-  let dy = ball1.y - ball2.y;
-  let distance = (dx * dx + dy * dy);
-  if(distance <= (ball1.r + ball2.r)*(ball1.r + ball2.r)){
-    collision = true;
-  }
-  return collision;
-}
-
-function collideBalls(ball1,ball2){
-  let dx = ball2.x - ball1.x;
-  let dy = ball2.y - ball1.y;
-  let distance = Math.sqrt(dx * dx + dy * dy);
-  let vCollisionNorm = {x: dx / distance, y:dy/distance}
-  let vRelativeVelocity = {x: ball1.dx - ball2.dx,y:ball1.dy - ball2.dy};
-  let speed = vRelativeVelocity.x * vCollisionNorm.x + vRelativeVelocity.y * vCollisionNorm.y;
-
-  if(speed < 0) return;
-
-  let impulse = 2 * speed / (ball1.r + ball2.r);
-
-  ball1.dx -= (impulse * ball2.r * vCollisionNorm.x);
-  ball1.dy -= (impulse * ball2.r * vCollisionNorm.y);
-  ball2.dx += (impulse * ball1.r * vCollisionNorm.x);
-  ball2.dy += (impulse * ball1.r * vCollisionNorm.y);
-
-  ball1.dy = (ball1.dy * ELASTICITY);
-  ball2.dy = (ball2.dy * ELASTICITY);
-}
-
-function collide(index) {
-  let ball = circleArr[index];
-  for(let j = index + 1; j < circleArr.length; j++){
-    let testBall = circleArr[j];
-    if(ballHitBall(ball,testBall)){
-      collideBalls(ball,testBall);
-    }
-  }
-}
+const inputs = document.querySelectorAll(".inputs-wrapper > input");
+inputs.forEach(input => {
+  input.addEventListener("focus", () => {
+    pauseCheckboxEl.checked = true;
+    inputIsFocused = true;
+  })
+  input.addEventListener("focusout", () => {
+    pauseCheckboxEl.checked = false;
+    inputIsFocused = false;
+  })
+})
 
 canvas.addEventListener("pointermove", (e) => {
   mousePos.x = e.clientX;
   mousePos.y = e.clientY;
 
-  circleArr.forEach((circle) => {
-    if (isAroundCircle(circle)) {
-      canvas.style.cursor = "grab";
-    } else {
-      canvas.style.cursor = "default";
-    }
-
+  for (let circle of circles) {
     if (circle.isHeld) {
       canvas.style.cursor = "grabbing";
-      circle.x = mousePos.x;
-      circle.y = mousePos.y;
+      break;
     }
-  })
+    if (circle.inCircle(mousePos)) {
+      canvas.style.cursor = "grab";
+      break;
+    }
+    canvas.style.cursor = "default";
+  }
 })
 
 canvas.addEventListener("pointerdown", (e) => {
   mousePos.x = e.clientX;
   mousePos.y = e.clientY;
 
-  circleArr.forEach((circle) => {
-    if (isAroundCircle(circle)) {
-      isPaused = true;
+  circles.forEach((circle, index) => {
+    if (circle.inCircle(mousePos)) {
+      selectedCircle = index;
       circle.isHeld = true;
       canvas.style.cursor = "grabbing";
+    } else {
+      circle.isHeld = false;
     }
   })
 })
 
 canvas.addEventListener("pointerup", () => {
-  circleArr.forEach((circle) => {
-    if (isAroundCircle(circle)) {
+  circles.forEach(circle => {
+    if (circle.inCircle(mousePos)) {
       canvas.style.cursor = "grab";
     }
     circle.isHeld = false;
   })
 })
 
-function isAroundCircle(circle) {
-  return (
-    mousePos.x <= circle.x + circle.r &&
-    mousePos.x >= circle.x - circle.r &&
-    mousePos.y <= circle.y + circle.r &&
-    mousePos.y >= circle.y - circle.r
-  );
-}
-
 function update() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  if (isPaused) {
-    pauseButtonEl.innerHTML = "Play"
-  } else {
-    pauseButtonEl.innerHTML = "Pause"
+  isPaused = pauseCheckboxEl.checked;
+  pauseCheckboxEl.checked = isPaused;
+
+  collisionEnabled = enableCollisionOptionEl.checked;
+  enableCollisionOptionEl.checked = collisionEnabled;
+
+  showHeading = showHeadingOptionEl.checked;
+  showHeadingOptionEl.checked = showHeading;
+
+  if (!inputIsFocused && selectedCircle !== null) {
+    vxInputEl.value = Math.round(circles[selectedCircle].vx * 10000) / 10000;
+    vyInputEl.value = Math.round(circles[selectedCircle].vy * 10000) / 10000;
   }
+
+  if (isPaused) {
+    playButtonEl.style.display = "none";
+    pauseButtonEl.style.display = "grid";
+  } else {
+    playButtonEl.style.display = "grid";
+    pauseButtonEl.style.display = "none";
+  }
+
+  circles.forEach((circle, index) => {
+    circle.update();
+    circle.collideWithWall();
+
+    if (collisionEnabled) {
+      let ball = circles[index];
+      for(let j = index + 1; j < circles.length; j++){
+        let testBall = circles[j];
+        if(ball.collideBetweenCircle(testBall)){
+          ball.collideWithOtherBall(testBall);
+        }
+      }
+    }
+
+    if (circle.isHeld) {
+      circle.x = mousePos.x;
+      circle.y = mousePos.y;
+      circle.vx = 0;
+      circle.vy = 0;
+
+      circle.lineUpdate();
+    }
+    circle.draw();
+  })
 }
 
 function gameLoop(timestamp) {
@@ -261,18 +371,6 @@ function gameLoop(timestamp) {
   oldTimestamp = timestamp;
 
   update();
-
-  circleArr.forEach((circle, index) => {
-    collideWithWall(circle);
-    circle.update();
-    if (collisionEnabled) {
-      collide(index);
-    }
-    circle.draw();
-  })
 }
 
-circleArr.forEach((circle) => {
-  circle.draw();
-})
 requestAnimationFrame(gameLoop);
